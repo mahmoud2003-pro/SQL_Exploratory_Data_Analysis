6c8eecc33765057448e30c84461d07e853b04c20
Mahmoud Eslam <mh35177273@gmail.com>
2026-02-22 00:35:59 +0200

    Add sales analysis by year and month
    This SQL script analyzes sales data by year and month, calculating total sales, total customers, and total quantity for each period.

---
+++ scripts/04_change_over_time_analysis.sql
@@ -0,0 +1,22 @@
+-- Analyse by the year
+SELECT
+YEAR(order_date) AS order_year,
+SUM(sales_amount) AS total_sales,
+COUNT(DISTINCT customer_key) AS total_customers,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)
+ORDER BY YEAR(order_date) ASC
+-------------------------------------------
+
+-- Analyse by the month
+SELECT
+MONTH(order_date) AS order_month,
+SUM(sales_amount) AS total_sales,
+COUNT(DISTINCT customer_key) AS total_customers,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY MONTH(order_date)
+ORDER BY MONTH(order_date) ASC
